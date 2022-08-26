2dfe22e749cc3080934a4c856cd03b7f8470decb
ThaiHV <thai.hv@vinayamato.com>
2022-08-26 18:10:51 +0700

    update for header and footer user

--- resources/views/include/user/footer.blade.php
+++ resources/views/include/user/footer.blade.php
@@ -1,12 +1,49 @@
 <footer class="footer">
     <div class="container footer__container">
         <div class="footer__nav">
-            <div class="nav__item">
-                <a href="">
+            <div class="nav__item active">
+                <a href="" class="nav__link">
                     <p class="nav-ic">
-                        <img src="{{ url('assets/img/user/common/ic_search.svg') }}" alt="">
+                        <img class="ic-brown" src="{{ asset('assets/img/user/common/ic_search.svg') }}" alt="">
+                        <img class="ic-white" src="{{ asset('assets/img/user/common/ic_search_white.svg') }}" alt="">
                     </p>
                     <p class="nav-txt">ホーム</p>
+                </a>
+            </div>
+            <div class="nav__item">
+                <a href="" class="nav__link">
+                    <p class="nav-ic">
+                        <img class="ic-brown" src="{{ asset('assets/img/user/common/ic_fire.svg') }}" alt="">
+                        <img class="ic-white" src="{{ asset('assets/img/user/common/ic_fire_white.svg') }}" alt="">
+                    </p>
+                    <p class="nav-txt">マイイベント</p>
+                </a>
+            </div>
+            <div class="nav__item">
+                <a href="" class="nav__link">
+                    <p class="nav-ic">
+                        <img class="ic-brown" src="{{ asset('assets/img/user/common/ic_plan.svg') }}" alt="">
+                        <img class="ic-white" src="{{ asset('assets/img/user/common/ic_plan_white.svg') }}" alt="">
+                    </p>
+                    <p class="nav-txt">企画</p>
+                </a>
+            </div>
+            <div class="nav__item">
+                <a href="" class="nav__link">
+                    <p class="nav-ic">
+                        <img class="ic-brown" src="{{ asset('assets/img/user/common/ic_message.svg') }}" alt="">
+                        <img class="ic-white" src="{{ asset('assets/img/user/common/ic_message_white.svg') }}" alt="">
+                    </p>
+                    <p class="nav-txt">通知</p>
+                </a>
+            </div>
+            <div class="nav__item">
+                <a href="" class="nav__link">
+                    <p class="nav-ic">
+                        <img class="ic-brown" src="{{ asset('assets/img/user/common/ic_bag.svg') }}" alt="">
+                        <img class="ic-white" src="{{ asset('assets/img/user/common/ic_bag_white.svg') }}" alt="">
+                    </p>
+                    <p class="nav-txt">マイページ</p>
                 </a>
             </div>
         </div>
--- resources/views/include/user/header.blade.php
+++ resources/views/include/user/header.blade.php
@@ -1,5 +1,10 @@
-<header>
-    <div class="header__container">
-
+<header class="header">
+    <div class="container header__container">
+        <div class="header__wrapper">
+            <a href="" class="btn-back">
+                <img class="ic-back" src="{{ asset('assets/img/user/common/ic_back.svg') }}" alt="">
+            </a>
+            <p class="header-title">開催済み一覧</p>
+        </div>
     </div>
 </header>
