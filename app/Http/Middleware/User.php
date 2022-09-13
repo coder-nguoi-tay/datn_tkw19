bbb9e798b9914b776d68bb2683572e3a96514d10
thaiminhtuan135 <tuana11ht@gmail.com>
2022-09-13 08:51:32 +0700

    fix and format code

---
+++ app/Http/Middleware/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class User
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            if (!Auth::guard('user')->check()) {
+                return redirect(route('my-event.index'));
+            }
+            return $next($request);
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
