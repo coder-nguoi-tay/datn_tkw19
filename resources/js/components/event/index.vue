3ee59a5f5abdced68e0dedb640cc72421fea663d
ThaiHV <thai.hv@vinayamato.com>
2022-09-30 22:10:37 +0700

    format code

--- resources/js/components/event/index.vue
+++ resources/js/components/event/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="event-list">
-    <div class="container event-list__container">
+  <div class="event-list h-100">
+    <div class="container event-list__container h-100">
       <div class="event-list__wrapper" v-if="haveDate">
         <div class="cat-list draft-list">
           <h3 class="event-cat-title">下書き中(2)</h3>
@@ -8,13 +8,17 @@
             <div class="event-item">
               <div class="txt-draft">(下書き)</div>
               <div class="last-save">最終保存日 20222/10/1</div>
-              <h2 class="event-title">取引先の50周年祝いの品を選んでください!! - 第2回 -</h2>
+              <h2 class="event-title">
+                取引先の50周年祝いの品を選んでください!! - 第2回 -
+              </h2>
               <a href="" class="event-link"></a>
             </div>
             <div class="event-item">
               <div class="txt-draft">(下書き)</div>
               <div class="last-save">最終保存日 20222/9/25</div>
-              <h2 class="event-title">取引先の50周年祝いの品を選んでください!!2</h2>
+              <h2 class="event-title">
+                取引先の50周年祝いの品を選んでください!!2
+              </h2>
               <a href="" class="event-link"></a>
             </div>
           </div>
@@ -25,29 +29,57 @@
             <div class="event-item">
               <div class="event-left">
                 <div class="event-img">
-                  <img src="/assets/img/user/event/hand_2.png" alt="" width="140">
+                  <img
+                    src="/assets/img/user/event/hand_2.png"
+                    alt=""
+                    width="140"
+                  />
                 </div>
                 <div class="avatar">
-                  <img src="/assets/img/user/event/avatar_6.png" alt="" width="25">
+                  <img
+                    src="/assets/img/user/event/avatar_6.png"
+                    alt=""
+                    width="25"
+                  />
                 </div>
               </div>
               <div class="event-right">
                 <div class="event-right__container">
                   <a href="" class="cat-name">お助け</a>
-                  <h2 class="event-title">取引先の50周年祝いの品を選んでください!! - 第2回 -</h2>
+                  <h2 class="event-title">
+                    取引先の50周年祝いの品を選んでください!! - 第2回 -
+                  </h2>
                   <div class="event-meta d-flex align-items-center flex-wrap">
-                  <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_period_2.svg" alt=""/><span>残り...</span><b>5日と<span class="txt-time">12</span>時間<span class="txt-time">32</span>分</b>
-                  </span>
                     <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_people_2.svg" alt=""/><b><span class="txt-number">3</span>人</b>
-                  </span>
+                      <img
+                        src="/assets/img/user/event/ic_period_2.svg"
+                        alt=""
+                      /><span>残り...</span
+                      ><b
+                        >5日と<span class="txt-time">12</span>時間<span
+                          class="txt-time"
+                          >32</span
+                        >分</b
+                      >
+                    </span>
                     <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_coin_2.svg" alt=""/><b>無料</b>
-                  </span>
+                      <img
+                        src="/assets/img/user/event/ic_people_2.svg"
+                        alt=""
+                      /><b><span class="txt-number">3</span>人</b>
+                    </span>
                     <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_reward_2.svg" alt=""/><b><span class="txt-number">40,000</span>円</b>
-                  </span>
+                      <img
+                        src="/assets/img/user/event/ic_coin_2.svg"
+                        alt=""
+                      /><b>無料</b>
+                    </span>
+                    <span class="meta-item">
+                      <img
+                        src="/assets/img/user/event/ic_reward_2.svg"
+                        alt=""
+                      /><b><span class="txt-number">40,000</span>円</b>
+                    </span>
                   </div>
                   <div class="event-tag">
                     <a href=""># 企業</a>
@@ -71,29 +103,52 @@
               <div class="event-item">
                 <div class="event-left">
                   <div class="event-img">
-                    <img src="/assets/img/user/event/animal.png" alt="" width="140">
+                    <img
+                      src="/assets/img/user/event/animal.png"
+                      alt=""
+                      width="140"
+                    />
                   </div>
                   <div class="avatar">
-                    <img src="/assets/img/user/event/avatar_6.png" alt="" width="25">
+                    <img
+                      src="/assets/img/user/event/avatar_6.png"
+                      alt=""
+                      width="25"
+                    />
                   </div>
                 </div>
                 <div class="event-right">
                   <div class="event-right__container">
                     <a href="" class="cat-name">お役立ち情報</a>
-                    <h2 class="event-title">お忍びデートスポットをリサーチしてください!!</h2>
+                    <h2 class="event-title">
+                      お忍びデートスポットをリサーチしてください!!
+                    </h2>
                     <div class="event-meta d-flex align-items-center flex-wrap">
-                    <span class="meta-item">
-                        <img src="/assets/img/user/event/ic_period_2.svg" alt=""/><span>残り...</span><b><span class="txt-time">3</span>日</b>
-                    </span>
                       <span class="meta-item">
-                        <img src="/assets/img/user/event/ic_people_2.svg" alt=""/><b><span class="txt-number">0</span>人</b>
-                    </span>
+                        <img
+                          src="/assets/img/user/event/ic_period_2.svg"
+                          alt=""
+                        /><span>残り...</span
+                        ><b><span class="txt-time">3</span>日</b>
+                      </span>
                       <span class="meta-item">
-                        <img src="/assets/img/user/event/ic_coin_2.svg" alt=""/><b><span class="txt-number">1,000</span>円</b>
-                    </span>
+                        <img
+                          src="/assets/img/user/event/ic_people_2.svg"
+                          alt=""
+                        /><b><span class="txt-number">0</span>人</b>
+                      </span>
                       <span class="meta-item">
-                        <img src="/assets/img/user/event/ic_reward_2.svg" alt=""/><b><span class="txt-number">100,000,000</span>円</b>
-                    </span>
+                        <img
+                          src="/assets/img/user/event/ic_coin_2.svg"
+                          alt=""
+                        /><b><span class="txt-number">1,000</span>円</b>
+                      </span>
+                      <span class="meta-item">
+                        <img
+                          src="/assets/img/user/event/ic_reward_2.svg"
+                          alt=""
+                        /><b><span class="txt-number">100,000,000</span>円</b>
+                      </span>
                     </div>
                     <div class="event-tag">
                       <a href=""># 企業</a>
@@ -116,29 +171,52 @@
               <div class="event-item">
                 <div class="event-left">
                   <div class="event-img">
-                    <img src="/assets/img/user/event/adventure.png" alt="" width="140">
+                    <img
+                      src="/assets/img/user/event/adventure.png"
+                      alt=""
+                      width="140"
+                    />
                   </div>
                   <div class="avatar">
-                    <img src="/assets/img/user/event/avatar_6.png" alt="" width="25">
+                    <img
+                      src="/assets/img/user/event/avatar_6.png"
+                      alt=""
+                      width="25"
+                    />
                   </div>
                 </div>
                 <div class="event-right">
                   <div class="event-right__container">
                     <a href="" class="cat-name">アドベンチャー</a>
-                    <h2 class="event-title">参加費1,000円で、世界中を飛び回る私を見つけたら賞金!!</h2>
+                    <h2 class="event-title">
+                      参加費1,000円で、世界中を飛び回る私を見つけたら賞金!!
+                    </h2>
                     <div class="event-meta d-flex align-items-center flex-wrap">
-                    <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_period_2.svg" alt=""/><span>残り...</span><b><span class="txt-time">7</span>日</b>
-                    </span>
                       <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_people_2.svg" alt=""/><b><span class="txt-number">0</span>人</b>
-                    </span>
+                        <img
+                          src="/assets/img/user/event/ic_period_2.svg"
+                          alt=""
+                        /><span>残り...</span
+                        ><b><span class="txt-time">7</span>日</b>
+                      </span>
                       <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_coin_2.svg" alt=""/><b><span class="txt-number">1,000</span>円</b>
-                    </span>
+                        <img
+                          src="/assets/img/user/event/ic_people_2.svg"
+                          alt=""
+                        /><b><span class="txt-number">0</span>人</b>
+                      </span>
                       <span class="meta-item">
-                      <img src="/assets/img/user/event/ic_reward_2.svg" alt=""/><b><span class="txt-number">9,999,999</span>円</b>
-                    </span>
+                        <img
+                          src="/assets/img/user/event/ic_coin_2.svg"
+                          alt=""
+                        /><b><span class="txt-number">1,000</span>円</b>
+                      </span>
+                      <span class="meta-item">
+                        <img
+                          src="/assets/img/user/event/ic_reward_2.svg"
+                          alt=""
+                        /><b><span class="txt-number">9,999,999</span>円</b>
+                      </span>
                     </div>
                     <div class="event-tag">
                       <a href=""># 企業</a>
@@ -160,14 +238,18 @@
           </div>
         </div>
       </div>
-      <div class="event-nodata" v-else>
-        <p class="nodata-title">まだ企画されたイベントがありません。<br>
-          早速あなたの企画を作成しましょう。</p>
-        <div class="img-nodata text-center">
-          <img src="/assets/img/user/event/no_data.png" alt="" width="407">
-        </div>
-        <div class="btn-nodata">
-          <a href="" class="btn-create-event">イベントを企画する</a>
+      <div class="event-nodata__wrapper h-100" v-else>
+        <div class="event-nodata">
+          <p class="nodata-title">
+            まだ企画されたイベントがありません。<br />
+            早速あなたの企画を作成しましょう。
+          </p>
+          <div class="img-nodata text-center">
+            <img src="/assets/img/user/event/no_data.png" alt="" />
+          </div>
+          <div class="btn-nodata">
+            <a href="#" class="btn-create-event">イベントを企画する</a>
+          </div>
         </div>
       </div>
     </div>
@@ -179,7 +261,7 @@ export default {
   props: ['data'],
   data: function () {
     return {
-      haveDate: false,
+      haveDate: true
     }
   },
   created() {},
