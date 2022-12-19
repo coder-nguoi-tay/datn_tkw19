e84bd3ec8e7bf6f50106a29d24cc2f96c472bc05
trieuvietduc <tdtazan112@gmail.com>
2022-12-19 14:39:13 +0700

    update paginate search job

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1273,70 +1273,7 @@
                 </div>
             </div>
         </div>
-        <main>
-
-            <h1>Pagination with Vanilla JavaScript</h1>
-            <ul id="paginated-list" aria-live="polite">
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-                <li>Item 6</li>
-                <li>Item 7</li>
-                <li>Item 8</li>
-                <li>Item 9</li>
-                <li>Item 10</li>
-                <li>Item 11</li>
-                <li>Item 12</li>
-                <li>Item 13</li>
-                <li>Item 14</li>
-                <li>Item 15</li>
-                <li>Item 16</li>
-                <li>Item 17</li>
-                <li>Item 18</li>
-                <li>Item 19</li>
-                <li>Item 20</li>
-                <li>Item 21</li>
-                <li>Item 22</li>
-                <li>Item 23</li>
-                <li>Item 24</li>
-                <li>Item 25</li>
-                <li>Item 26</li>
-                <li>Item 27</li>
-                <li>Item 28</li>
-                <li>Item 29</li>
-                <li>Item 30</li>
-                <li>Item 31</li>
-                <li>Item 32</li>
-                <li>Item 33</li>
-                <li>Item 34</li>
-                <li>Item 35</li>
-                <li>Item 36</li>
-                <li>Item 37</li>
-                <li>Item 38</li>
-                <li>Item 39</li>
-                <li>Item 40</li>
-                <li>Item 41</li>
-                <li>Item 42</li>
-                <li>Item 43</li>
-                <li>Item 44</li>
-                <li>Item 45</li>
-                <li>Item 46</li>
-                <li>Item 47</li>
-                <li>Item 48</li>
-                <li>Item 49</li>
-                <li>Item 50</li>
-            </ul>
-
-            <nav class="pagination-container">
-
-                <div id="pagination-numbers">
-
-                </div>
-            </nav>
-        </main>
-        {{-- @if ($user->getProfileUse == null)
+        @if ($user->getProfileUse == null)
             <div class="modal fade" id="checkmodalProfile" tabindex="-1" role="dialog"
                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-xl" role="document">
@@ -1374,73 +1311,6 @@
         <script type="text/javascript">
             $(document).ready(function() {
                 $("#checkmodalProfile").modal('show');
-            });
-        </script> --}}
-        <script type="text/javascript">
-            $(document).ready(function() {
-                const paginationNumbers = document.getElementById("pagination-numbers");
-                const paginatedList = document.getElementById("paginated-list");
-                const listItems = paginatedList.querySelectorAll("li");
-
-                const paginationLimit = 10;
-                const pageCount = Math.ceil(listItems.length / paginationLimit);
-                let currentPage = 1;
-
-                const appendPageNumber = (index) => {
-                    const pageNumber = document.createElement("button");
-                    pageNumber.className = "pagination-number";
-                    pageNumber.innerHTML = index;
-                    pageNumber.setAttribute("page-index", index);
-                    pageNumber.setAttribute("aria-label", "Page " + index);
-
-                    paginationNumbers.appendChild(pageNumber);
-                };
-
-                const getPaginationNumbers = () => {
-                    for (let i = 1; i <= pageCount; i++) {
-                        appendPageNumber(i);
-                    }
-                };
-
-                const handleActivePageNumber = () => {
-                    document.querySelectorAll(".pagination-number").forEach((button) => {
-                        button.classList.remove("active");
-                        const pageIndex = Number(button.getAttribute("page-index"));
-                        if (pageIndex == currentPage) {
-                            button.classList.add("active");
-                        }
-                    });
-                };
-
-                const setCurrentPage = (pageNum) => {
-                    currentPage = pageNum;
-                    handleActivePageNumber();
-
-                    const prevRange = (pageNum - 1) * paginationLimit;
-                    const currRange = pageNum * paginationLimit;
-
-                    listItems.forEach((item, index) => {
-                        item.classList.add("hidden");
-                        if (index >= prevRange && index < currRange) {
-                            item.classList.remove("hidden");
-                        }
-                    });
-                };
-
-                window.addEventListener("load", () => {
-                    getPaginationNumbers();
-                    setCurrentPage(1);
-
-                    document.querySelectorAll(".pagination-number").forEach((button) => {
-                        const pageIndex = Number(button.getAttribute("page-index"));
-
-                        if (pageIndex) {
-                            button.addEventListener("click", () => {
-                                setCurrentPage(pageIndex);
-                            });
-                        }
-                    });
-                });
             });
         </script>
     </body>
