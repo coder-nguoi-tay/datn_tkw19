9973b6f7fdbd32c34147286e24ba0d3745eee231
khanh3092002 <108168376+voanlaptrinh@users.noreply.github.com>
2023-01-05 21:26:49 +0700

    newsblade new

--- resources/views/client/detai-job.blade.php
+++ resources/views/client/detai-job.blade.php
@@ -195,15 +195,7 @@
                                             <li><a href="javascript:void(0);"><i class="ti-instagram"></i></a></li>
                                         </ul>
                                     </div>
-                                    <div class="jbd-02-aply">
-                                        <div class="jbl_button mb-2">
-                                            <a href="#" class="btn btn-md rounded gray fs-sm ft-medium mr-2">lưu
-                                                Job</a>
-                                            <a href="#"
-                                                class="btn btn-md rounded theme-bg text-light fs-sm ft-medium">Ứng tuyển
-                                                ngay</a>
-                                        </div>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -285,7 +277,7 @@
                             <span class="content">{{ $jobCompany[0]->address }}</span>
                         </div>
                     </div>
-                    <div class="box-job-company">
+                    <div class="box-job-company pb-10">
                         <div class="box-title">
                             <h2 class="box-name"> <span style="margin-left: 5px !important;">Việc làm cùng công
                                     ty</span></h2>
@@ -297,7 +289,7 @@
                                 <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
                                     @foreach ($jobCompany as $item)
                                         <div class="col">
-                                            <div class="p-3  bg-light box-showdow-company">
+                                            <div class="p-3 bg-light box-showdow-company">
                                                 <div class="position-absolute ab-right"><button type="button"
                                                         class="p-3 border circle d-flex align-items-center justify-content-center bg-white text-gray"
                                                         title="123123213"><i
@@ -337,8 +329,226 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </section>
+        {{-- <section class="space min container-relate container container-detai p-3">
+            <div class="row">
+                <div class="col-8">
+                    <div class="box-title">
+                        <h2 class="box-name"><span style="margin-left: 5px !important">Việc làm liên quan</span>
+                        </h2>
+                    </div>
+                    @foreach ($rules as $item)
+                        <div class="job_grid d-block border rounded px-3 pt-3 pb-2">
+                            <div class="jb-list01-flex d-flex align-items-start justify-content-start">
+                                <div class="jb-list01-thumb">
+                                    <img src="{{ asset($item->logo) }}" class="img-fluid circle" width="90"
+                                        alt="">
+                                </div>
+
+                                <div class="jb-list01 pl-3">
+                                    <div class="jb-list-01-title">
+                                        <h5 class="ft-medium mb-1"><a
+                                                href="{{ route('home.detail.show', [$item->title, $item->id]) }}">{{ $item->title }}<img
+                                                    src="assets/img/verify.svg" class="ml-1" width="12"
+                                                    alt=""></a></h5>
+                                    </div>
+                                    <div class="jb-list-01-info d-block mb-3">
+                                        <span class="text-muted mr-2"><i
+                                                class="lni lni-map-marker mr-1"></i>{{ $item->getlocation->name }}</span>
+                                        <span class="text-muted mr-2"><i
+                                                class="lni lni-tag mr-1"></i>{{ $item->getprofession->name }}</span>
+                                        <span class="text-muted mr-2"><i
+                                                class="lni lni-briefcase mr-1"></i>{{ $item->getTime_work->name }}</span>
+                                        <span class="text-muted mr-2"><i
+                                                class="lni lni-graduation mr-1"></i>{{ $item->getExperience->name }}</span>
+                                    </div>
+                                    <div class="jb-list-01-title d-inline">
+                                        @foreach ($item->getskill as $item)
+                                            <span
+                                                class="mr-2 mb-2 d-inline-flex px-2 py-1 rounded theme-cl theme-bg-light">{{ $item->name }}</span>
+                                        @endforeach
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="col-4">
+                    <div class="bg-white rounded mb-4 box-text-search">
+
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.863855881457!2d105.74459841535469!3d21.038132792833228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2sFPT%20Polytechnic%20Hanoi!5e0!3m2!1sen!2s!4v1672849269578!5m2!1sen!2s"
+                            width="100%"  style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <!-- Find New Property -->
+
+                    </div>
+                </div>
+            </div>
+
+        </section> --}}
+        <div class="space min container-relate container container-detai p-3" id="tab-job">
+            <div class="box-job-similar  box-white">
+                <h2 class="box-name"> <span style="margin-left: 5px !important;">Việc làm liên quan</span> </h2>
+                <div class="box-content">
+                    <div class="job-body row">
+                        <div class="col-md-9">
+                            <div id="box-relate-jobs" class="box_general">
+                                <div class="" id="relate-jobs">
+                                    @foreach ($rules as $item)
+                                        <div class="job_grid d-block border rounded px-3 pt-3 pb-2">
+                                            <div class="jb-list01-flex d-flex align-items-start justify-content-start">
+                                                <div class="jb-list01-thumb">
+                                                    <img src="{{ asset($item->logo) }}" class="img-fluid circle"
+                                                        width="90" alt="">
+                                                </div>
+
+                                                <div class="jb-list01 pl-3">
+                                                    <div class="jb-list-01-title">
+                                                        <h5 class="ft-medium mb-1"><a
+                                                                href="{{ route('home.detail.show', [$item->title, $item->id]) }}">{{ $item->title }}<img
+                                                                    src="assets/img/verify.svg" class="ml-1"
+                                                                    width="12" alt=""></a></h5>
+                                                    </div>
+                                                    <div class="jb-list-01-info d-block mb-3">
+                                                        <span class="text-muted mr-2"><i
+                                                                class="lni lni-map-marker mr-1"></i>{{ $item->getlocation->name }}</span>
+                                                        <span class="text-muted mr-2"><i
+                                                                class="lni lni-tag mr-1"></i>{{ $item->getprofession->name }}</span>
+                                                        <span class="text-muted mr-2"><i
+                                                                class="lni lni-briefcase mr-1"></i>{{ $item->getTime_work->name }}</span>
+                                                        <span class="text-muted mr-2"><i
+                                                                class="lni lni-graduation mr-1"></i>{{ $item->getExperience->name }}</span>
+                                                    </div>
+                                                    <div class="jb-list-01-title d-inline">
+                                                        @foreach ($item->getskill as $item)
+                                                            <span
+                                                                class="mr-2 mb-2 d-inline-flex px-2 py-1 rounded theme-cl theme-bg-light">{{ $item->name }}</span>
+                                                        @endforeach
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+
+
+
+                                    {{-- <div data-job-id="895139"
+                                        data-jr-id="job-graph-v2::1672847954248-7a12fd::e3defc0a91aa4575bf55a81d0bd46a1c::15"
+                                        data-job-position="15" class="job-item job-ta result-job-hover">
+                                        <div class="avatar"><a
+                                                href="https://www.topcv.vn/viec-lam/lap-trinh-vien-php-wordpress/895139.html?ta_source=SuggestSimilarJob&amp;jr_i=job-graph-v2%3A%3A1672847954248-7a12fd%3A%3Ae3defc0a91aa4575bf55a81d0bd46a1c%3A%3A15"
+                                                target="_blank" lass="company-logo"><img loading="lazy"
+                                                    src="https://www.topcv.vn/v4/image/topcv-logo-company-default.png"
+                                                    alt="Công ty Cổ phần Công nghệ Phần mềm Tinh Vân"
+                                                    title="Lập Trình Viên PHP (Wordpress)" class="w-100"></a></div>
+                                        <div class="body">
+                                            <div class="content">
+                                                <div class="ml-auto">
+                                                    <h3 class="title">
+                                                        <!----> <a target="_blank"
+                                                            href="https://www.topcv.vn/viec-lam/lap-trinh-vien-php-wordpress/895139.html?ta_source=SuggestSimilarJob&amp;jr_i=job-graph-v2%3A%3A1672847954248-7a12fd%3A%3Ae3defc0a91aa4575bf55a81d0bd46a1c%3A%3A15"
+                                                            class="underline-box-job"><span data-toggle="tooltip"
+                                                                title="" data-placement="top" data-container="body"
+                                                                class="bold transform-job-title"
+                                                                data-original-title="Lập Trình Viên PHP (Wordpress)">Lập
+                                                                Trình Viên PHP (Wordpress)</span> <span
+                                                                class="icon-verified-employer level-five"><i
+                                                                    data-toggle="tooltip" data-html="true" title=""
+                                                                    data-placement="top" class="fa-solid fa-circle-check"
+                                                                    data-original-title="
+                                                            <b>Nhà tuyển dụng</b><span> đã được xác thực:</span><br>
+                                                            <span><i class='fa-solid fa-circle-check color-green'></i> Đã xác thực email tên miền công ty</span><br>
+                                                            <span><i class='fa-solid fa-circle-check color-green'></i> Đã xác thực số điện thoại</span><br>
+                                                            <span><i class='fa-solid fa-circle-check color-green'></i> Đã được duyệt Giấy phép kinh doanh</span><br>
+                                                            <span><i class='fa-solid fa-circle-check color-green'></i> Tài khoản NTD được tạo tối thiểu 6 tháng</span><br>
+                                                            <span><i class='fa-solid fa-circle-check color-green'></i> Chưa có lịch sử bị báo cáo tin đăng</span><br>"></i></span></a>
+                                                    </h3>
+                                                    <p class="company underline-box-job"><a
+                                                            href="https://www.topcv.vn/cong-ty/cong-ty-co-phan-cong-nghe-phan-mem-tinh-van/102136.html"
+                                                            data-toggle="tooltip" title="" data-placement="top"
+                                                            data-container="body" target="_blank"
+                                                            data-original-title="Công ty Cổ phần Công nghệ Phần mềm Tinh Vân">Công
+                                                            ty Cổ phần Công nghệ Phần mềm Tinh Vân</a></p>
+                                                </div>
+                                                <div class="mr-auto text-right">
+                                                    <p class="deadline">Còn <b>14</b> ngày để ứng tuyển</p>
+                                                    <!---->
+                                                    <!---->
+                                                    <!---->
+                                                    <!---->
+                                                </div>
+                                            </div>
+                                            <div class="d-flex">
+                                                <div class="label-content ml-auto"><label class="salary">15-25
+                                                        triệu</label> <label data-toggle="tooltip" data-html="true"
+                                                        title="" data-placement="top" data-container="body"
+                                                        class="address"
+                                                        data-original-title="<p style='text-align: left'>Hồ Chí Minh: Quận 1</p>">Hồ
+                                                        Chí Minh</label> <label class="time">2 tuần trước</label></div>
+                                                <div class="icon mr-auto">
+                                                    <div id="box-save-job-895139"
+                                                        class="box-save-job box-save-job-hover job-notsave"><a
+                                                            href="javascript:void(0)" data-id="895139"
+                                                            data-title="Lập Trình Viên PHP (Wordpress)"
+                                                            class="btn-save save"><span id="save-job-loading"
+                                                                style="display: none;"><img
+                                                                    src="https://www.topcv.vn/v3/images/ap-loading.gif"
+                                                                    style="width: 20px;"></span> <i
+                                                                class="fa-light fa-heart"></i></a> <a
+                                                            href="javascript:void(0)" data-toggle="tooltip"
+                                                            title="" data-id="895139"
+                                                            data-title="Lập Trình Viên PHP (Wordpress)"
+                                                            data-placement="top" data-container="body"
+                                                            class="btn-unsave unsave text-highlight"
+                                                            data-original-title="Bỏ lưu"><span id="unsave-job-loading"
+                                                                style="display: none;"><img
+                                                                    src="https://www.topcv.vn/v3/images/ap-loading.gif"
+                                                                    style="width: 20px;"></span> <i
+                                                                class="fa-solid fa-heart"></i></a></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> --}}
+
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 right-box">
+                            <div class="interested">
+                                <div class="box box-white box-no-padding" id="company-spotlight">
+
+                                    <div class="spotlight-company">
+                                        <div class="spotlight-image">
+
+                                            <div class="spotlight-cover-wraper">
+
+                                                <iframe
+                                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.863855881457!2d105.74459841535469!3d21.038132792833228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2sFPT%20Polytechnic%20Hanoi!5e0!3m2!1sen!2s!4v1672850305456!5m2!1sen!2s"
+                                                    width="300px" height="300px" style="border:0;" allowfullscreen=""
+                                                    loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                                                    class="spotlight-cover"></iframe>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- Log In Modal -->
         <div class="modal fade" id="exampleModallogin" tabindex="-1" role="dialog"
             aria-labelledby="exampleModalLabel" aria-hidden="true">
