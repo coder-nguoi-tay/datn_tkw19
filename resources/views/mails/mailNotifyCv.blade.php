db5529deea58550e7b259a1d2cc142a581a694f1
hungnp19072002 <hungnv19072002@gmail.com>
2023-01-15 14:48:48 +0700

    update send mail

---
+++ resources/views/mails/mailNotifyCv.blade.php
@@ -0,0 +1,36 @@
+<div class=" box-send-mail"
+    style=" margin: 0;
+            padding: 25px;
+            background-color: rgb(243, 243, 243);
+            width: 100%;
+            height: auto;">
+
+    <div class=" box-mail"
+        style=" background: white;
+            margin: 40px 0px 40px 0px;
+            width: 80%;
+            height: 500px;
+            margin-left: 10%;
+            text-align: justify;">
+        <br>
+        <img src="/public/assets/img/logo_it.jpg" alt="logo" class="logo"
+            style="width: 150px; height: 50px; margin-left: 42%;" />
+        <br>
+        <div class="mail-content" style="  padding: 30px;">
+            <p>Kính gửi Quý khách hàng sssss</p>
+            <p>TopCV xin thông báo, tài khoản nhà tuyển dụng của Quý khách chưa xác thực số điện thoại.</p>
+
+            <p>Để bảo vệ tài khoản nhà tuyển dụng của mình và trải nghiệm dịch vụ của TopCV một cách tốt
+                nhất,
+                Quý
+                khách vui lòng nhấn
+                vào đây để tiến hành xác thực.</p>
+            <p>Trong trường hợp cần hỗ trợ thêm thông tin, Quý khách vui lòng liên hệ:</p>
+
+            <p>Tổng đài CSKH: SĐT: (024) 7107 9799</p>
+            <p style="text-align: center">Email CSKH: cskh@topcv.vn</p>
+            <p>Trân trọng,</p>
+        </div>
+    </div>
+
+</div>
