df598918ff6507261705c59f1451eb6d633ce514
trieuvietduc <tdtazan112@gmail.com>
2023-01-17 20:50:34 +0700

    fix job for user

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -135,7 +135,7 @@
                                                                                                     -webkit-box-orient: vertical;
                                                                                                     overflow: hidden;
                                                                                                     text-overflow: ellipsis;">
-                                                                        <a href="/home/detail/{{  $item->id }}"
+                                                                        <a href="/home/detail/{{ $item->id }}"
                                                                             data-toggle="tooltip" title=""
                                                                             data-placement="top" data-container="body"
                                                                             data-original-title="{{ $item->nameCompany }}">{{ $item->title }}</a>
@@ -540,7 +540,7 @@
                                                                                                     overflow: hidden;
                                                                                                     text-overflow: ellipsis;">
                                                                         <a
-                                                                            href="home/detail/{{  $item->id }}">{{ $item->title }}</a>
+                                                                            href="home/detail/{{ $item->id }}">{{ $item->title }}</a>
                                                                     </h6>
                                                                     <div class="d-flex flex-wrap gap-15 mt-5">
                                                                         <span><i
@@ -706,159 +706,6 @@
                     </div>
                 </div>
             </section>
-            {{-- @if (Auth::guard('user')->check())
-                <section
-                    class="elementor-section elementor-top-section elementor-element elementor-element-d98c6ff elementor-section-boxed elementor-section-height-default elementor-section-height-default"
-                    data-id="d98c6ff" data-element_type="section">
-                    <div class="elementor-container elementor-column-gap-no">
-                        <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-1c22a67"
-                            data-id="1c22a67" data-element_type="column">
-                            <div class="elementor-widget-wrap elementor-element-populated">
-                                <div class="elementor-element elementor-element-ed0a5e5 m-0 elementor-widget elementor-widget-title"
-                                    data-id="ed0a5e5" data-element_type="widget" data-widget_type="title.default">
-                                    <div class="elementor-widget-container">
-                                        <h2 class="control-heading-title m-0">Việc làm phù hợp với bạn</h2>
-                                    </div>
-                                </div>
-                                <div class="elementor-element elementor-element-54a362e text-center elementor-widget elementor-widget-template"
-                                    data-id="54a362e" data-element_type="widget" data-widget_type="template.default">
-                                    <div class="elementor-widget-container">
-
-                                        <div class="list-tabs list-tabs-2 mt-30">
-                                            <ul class="nav nav-tabs ps-0" role="tablist">
-                                                <li>
-                                                    <a class="active" id="nav-tab-content-writer"
-                                                        href="#tab-content-writer" data-bs-toggle="tab" role="tab"
-                                                        aria-controls="tab-content-writer" aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/security-icon.png"
-                                                            width="18" alt="Content Writer">
-                                                        Content Writer </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-finance" href="#tab-finance"
-                                                        data-bs-toggle="tab" role="tab" aria-controls="tab-finance"
-                                                        aria-selected="true">
-                                                        <img decoding="async" src="../wp-content/uploads/2022/10/bank.png"
-                                                            width="18" alt="Finance">
-                                                        Finance </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-human-resource"
-                                                        href="#tab-human-resource" data-bs-toggle="tab" role="tab"
-                                                        aria-controls="tab-human-resource" aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/human-resource.png"
-                                                            width="18" alt="Human Resource">
-                                                        Human Resource </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-management" href="#tab-management"
-                                                        data-bs-toggle="tab" role="tab"
-                                                        aria-controls="tab-management" aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/management-icon.png"
-                                                            width="18" alt="Management">
-                                                        Management </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-market-research"
-                                                        href="#tab-market-research" data-bs-toggle="tab" role="tab"
-                                                        aria-controls="tab-market-research" aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/research-icon.png"
-                                                            width="18" alt="Market Research">
-                                                        Market Research </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-marketing-sale"
-                                                        href="#tab-marketing-sale" data-bs-toggle="tab" role="tab"
-                                                        aria-controls="tab-marketing-sale" aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/career-icon.png"
-                                                            width="18" alt="Marketing &amp; Sale">
-                                                        Marketing &amp; Sale </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-retail-products"
-                                                        href="#tab-retail-products" data-bs-toggle="tab" role="tab"
-                                                        aria-controls="tab-retail-products" aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/retail-icon.png"
-                                                            width="18" alt="Retail &amp; Products">
-                                                        Retail &amp; Products </a>
-                                                </li>
-                                                <li>
-                                                    <a class="" id="nav-tab-software" href="#tab-software"
-                                                        data-bs-toggle="tab" role="tab" aria-controls="tab-software"
-                                                        aria-selected="true">
-                                                        <img decoding="async"
-                                                            src="../wp-content/uploads/2022/10/lamp-icon.png"
-                                                            width="18" alt="Software">
-                                                        Software </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div class="mt-10">
-                                            <div class="tab-content" id="myTabContent-2">
-                                                <div class="tab-pane fade show active" id="tab-content-writer"
-                                                    role="tabpanel" aria-labelledby="tab-content-writer">
-                                                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 text-start">
-                                                        @foreach ($jobForUser as $item)
-                                                            <div class="col">
-                                                                <div class="card-grid-2 grid-bd-16 hover-up">
-                                                                    <div class="card-block-info pt-25">
-                                                                        <h6><a
-                                                                                href="home/detail/{{ $item->title . '-' . $item->id }}">{{ $item->title }}</a>
-                                                                        </h6>
-                                                                        <div class="d-flex flex-wrap gap-15 mt-5">
-                                                                            <span><i
-                                                                                    class="fi-rr-briefcase ms-0 me-5"></i>{{ $item->getTime_work->name }}</span>
-                                                                            <span><i
-                                                                                    class="fi-rr-clock ms-0 me-5"></i><time
-                                                                                    datetime="2022-09-27">{{ $item->end_job_time }}</time></span>
-                                                                        </div>
-
-                                                                        <div class="card-2-bottom mt-20">
-                                                                            <div class="row">
-                                                                                <div class="col-lg-8 col-md-8">
-                                                                                    <div class="d-flex gap-15">
-                                                                                        <img decoding="async"
-                                                                                            class="rounded-circle"
-                                                                                            src="../wp-content/uploads/2022/09/brand-7.png"
-                                                                                            style="width: 35px; height: 35px"
-                                                                                            alt="Ashford">
-                                                                                        <div class="info-right-img">
-                                                                                            <h6
-                                                                                                class="color-brand-1 lh-14 mb-0">
-                                                                                                {{ $item->nameCompany }}
-                                                                                            </h6>
-                                                                                            <span
-                                                                                                class="card-location font-xxs pl-15 color-text-paragraph-2">{{ $item->getlocation->name }}</span>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="col-lg-4 col-md-4 text-end">
-                                                                                    <span
-                                                                                        class="card-text-price">{{ $item->getwage->name }}</span>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        @endforeach
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            @endif --}}
             <section
                 class="elementor-section elementor-top-section elementor-element elementor-element-acd8c1d elementor-section-boxed elementor-section-height-default elementor-section-height-default">
                 <div class="container">
@@ -971,6 +818,42 @@
             </div>
         </section>
     </main>
+    @if (Auth::guard('user')->check())
+        @if ($user->getProfileUse == null)
+            <div class="modal fade" id="checkmodalProfile" tabindex="-1" role="dialog"
+                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <job-target
+                                :data="{{ json_encode([
+                                    'lever' => $lever,
+                                    'experience' => $experience,
+                                    'wage' => $wage,
+                                    'skill' => $skill,
+                                    'timework' => $timework,
+                                    'profession' => $profession,
+                                    'majors' => $majors,
+                                    'location' => $location,
+                                    'workingform' => $workingform,
+                                    'user' => $user,
+                                    'urlStore' => route('profile.updateProfile'),
+                                    'getskill' => $getskill,
+                                ]) }}">
+                            </job-target>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    $("#checkmodalProfile").modal('show');
+                });
+            </script>
+        @endif
+    @endif
     <div class="modal fade" id="controlJobManagerRegister" tabindex="-1" role="dialog"
         aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
