b500247cda82572ee5134954e6af92c4fbd9bccf
khanh3092002 <108168376+voanlaptrinh@users.noreply.github.com>
2023-01-17 21:51:08 +0700

    a

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,6 @@
 <link href="{{ asset('assets/css/404.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <div class="site">
     <div class="sketch">
         <div class="bee-sketch red"></div>
@@ -6,6 +8,7 @@
     </div>
 
     <h1>404:
-        <small>Players Not Found</small>
+        <small>Players Not Found</small><br>
+        <a href="{{ route('index') }}"> <button class="btn btn-success"> back</button></a>
     </h1>
 </div>
